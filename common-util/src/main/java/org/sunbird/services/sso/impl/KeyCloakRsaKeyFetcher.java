04b138bc0ae22e035dac6ec44a4d723a7e0e7d73
Mayank Morya <moryamayank9@gmail.com>
2018-10-05 15:11:30 +0530

    removed "/auth"  as it is already present in url
    updated url as it was forming http://example.com/auth/auth/realms/" + realm + "/protocol/openid-connect/certs"
    remove extra auth new url will be http://example.com/auth/realms/" + realm + "/protocol/openid-connect/certs"

--- common-util/src/main/java/org/sunbird/services/sso/impl/KeyCloakRsaKeyFetcher.java
+++ common-util/src/main/java/org/sunbird/services/sso/impl/KeyCloakRsaKeyFetcher.java
@@ -35,25 +35,29 @@ public class KeyCloakRsaKeyFetcher {
 
   public PublicKey getPublicKeyFromKeyCloak(String url, String realm) {
     try {
+      Map<String, String> valueMap = new HashMap<String,String>();
       Decoder urlDecoder = Base64.getUrlDecoder();
       KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-      Map<String, String> valueMap = getValuesFromJson(requestKeyFromKeycloak(url, realm));
-      BigInteger modulus = new BigInteger(1, urlDecoder.decode(valueMap.get("modulusBase64")));
-      BigInteger publicExponent = new BigInteger(1, urlDecoder.decode(valueMap.get("exponentBase64")));
-      PublicKey key = keyFactory.generatePublic(new RSAPublicKeySpec(modulus, publicExponent));
-      saveToCache(key);
-      return key;
-    } catch (NoSuchAlgorithmException e) {
-      ProjectLogger.log(" Got NoSuchAlgorithmException " + e);
-      return null;
-    } catch (InvalidKeySpecException e) {
-      ProjectLogger.log(" Got InvalidKeySpecException " + e);
-      return null;
+      String publicKeyString = requestKeyFromKeycloak(url, realm);
+      if (publicKeyString != null) {
+         valueMap = getValuesFromJson(publicKeyString);
+        if (valueMap != null) {
+          BigInteger modulus = new BigInteger(1, urlDecoder.decode(valueMap.get(modulusBase64)));
+          
+          BigInteger publicExponent =
+              new BigInteger(1, urlDecoder.decode(valueMap.get(exponentBase64)));
+          PublicKey key = keyFactory.generatePublic(new RSAPublicKeySpec(modulus, publicExponent));
+          saveToCache(key);
+          return key;
+        }
+      }
     } catch (Exception e) {
-      ProjectLogger.log("Got Errors while getting public key " + e);
-      return null;
+      ProjectLogger.log(
+          "KeyCloakRsaKeyFetcher:getPublicKeyFromKeyCloak: Exception occurred with message = "
+              + e.getMessage(),
+          LoggerEnum.ERROR);
     }
-
+    return null;
   }
 
   private void saveToCache(PublicKey key) {
@@ -66,7 +70,7 @@ public class KeyCloakRsaKeyFetcher {
   private String requestKeyFromKeycloak(String url, String realm) {
 
     HttpClient client = HttpClientBuilder.create().build();
-    HttpGet request = new HttpGet(url + "/auth/realms/" + realm + "/protocol/openid-connect/certs");
+    HttpGet request = new HttpGet(url + "/realms/" + realm + "/protocol/openid-connect/certs");
 
     try {
       HttpResponse response = client.execute(request);
@@ -86,23 +90,26 @@ public class KeyCloakRsaKeyFetcher {
 
   private Map<String, String> getValuesFromJson(String response) {
     JsonParser parser = new JsonParser();
-    Map<String, String> values = new HashMap<>();
+    Map<String, String> values = new HashMap<String,String>();
     JsonObject json = (JsonObject) parser.parse(response);
     try {
       Object key = json.get("keys");
       if (key != null) {
         JsonArray value = (JsonArray) parser.parse(key.toString());
         JsonObject v = (JsonObject) value.get(0);
-        values.put("modulusBase64", v.get("n").toString());
-        values.put("exponentBase64", v.get("e").toString());
-
+        values.put(modulusBase64, v.get("n").getAsString().toString());
+        values.put(exponentBase64, v.get("e").getAsString().toString());
       }
+      
     } catch (NullPointerException e) {
-      ProjectLogger.log(" NULL values returned from keycloak server for sso_public keys ");
+      ProjectLogger.log(
+          "KeyCloakRsaKeyFetcher:getValuesFromJson: Exception occurred with message = "
+              + e.getMessage(),
+          LoggerEnum.ERROR);
+      return null;
     }
 
     return values;
   }
-
 
 }
